b05232583fdd6b3d82ee69fdf83bd119ed24a90d
Takayuki Kaburagi <t.kaburagi@me.com>
2020-04-15 16:42:08 +0900

    first commit

---
+++ src/main/java/run/kabuctl/demo/DemoController.java
@@ -0,0 +1,13 @@
+package run.kabuctl.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+}
